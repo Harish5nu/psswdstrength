bc731234066a969adedf33eb93350d44833396f4
Harish Saud <saudharish770@gmail.com>
2025-04-01 21:18:01 +0545

    Add files via upload
    simple password strength checker

---
+++ passwdstg.py
@@ -0,0 +1,28 @@
+import re
+
+def check_password_strength(password):
+    strength = 0
+    remarks = ""
+    
+    if len(password) >= 8:
+        strength += 1  # Length Check
+    if re.search(r"[A-Z]", password):
+        strength += 1  # Uppercase Letter
+    if re.search(r"[a-z]", password):
+        strength += 1  # Lowercase Letter
+    if re.search(r"\d", password):
+        strength += 1  # Digit Check
+    if re.search(r"[!@#$%^&*()_+]", password):
+        strength += 1  # Special Characters
+    
+    if strength == 5:
+        remarks = "Very Strong 💪"
+    elif strength >= 3:
+        remarks = "Medium 👍"
+    else:
+        remarks = "Weak ⚠️"
+    
+    return f"Password Strength: {remarks}"
+
+password = input("Enter Password: ")
+print(check_password_strength(password))
